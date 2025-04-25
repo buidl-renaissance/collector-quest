7b4c84960b8a796effacdc047b59b6c4bf4f3e12
John Gulbronson <john@benzinga.com>
2025-04-25 14:00:11 -0400

    workshop page

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,11 +2,20 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { WalletProvider } from "@suiet/wallet-kit";
 import "@suiet/wallet-kit/style.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <WalletProvider>
-      <Component {...pageProps} />
-    </WalletProvider>
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <WalletProvider>
+        <Component {...pageProps} />
+      </WalletProvider>
+    </>
   );
 }
