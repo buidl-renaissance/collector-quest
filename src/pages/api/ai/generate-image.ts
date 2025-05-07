0461c476e3812297d708dbfdf3c47caf8ae4cff7
John Gulbronson <john@benzinga.com>
2025-05-07 08:36:56 -0400

    use remote images

--- src/pages/api/ai/generate-image.ts
+++ src/pages/api/ai/generate-image.ts
@@ -1,10 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import OpenAI from "openai";
-import fs from "fs";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const getImage = async (image: string) => {
+  if (image.startsWith('data:image')) {
+    const base64Data = image.split(',')[1];
+    const imageBuffer = Buffer.from(base64Data, 'base64');
+    return new File([imageBuffer], "image.png", { type: "image/png" });
+  } else if (image.startsWith('http')) {
+    const response = await fetch(image);
+    const arrayBuffer = await response.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    return new File([buffer], "image.png", { type: response.headers.get("content-type") || "image/png" });
+  }
+  return new File([], "image.png", { type: "image/png" });
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -23,36 +36,20 @@ export default async function handler(
 
 
     let imageFile: File = new File([], "image.png", { type: "image/png" });
-    const maskPath = "images/mask.png";
-    const maskBuffer = fs.readFileSync(maskPath);
-    const mask = new File([maskBuffer], "mask.png", { type: "image/png" });
+    const maskFile = await getImage("https://collectorquest.ai/images/mask.png");
 
     // Process the provided image if it exists
     if (image) {
-      if (image.startsWith('data:image')) {
-        // Handle base64 encoded image
-        const base64Data = image.split(',')[1];
-        const imageBuffer = Buffer.from(base64Data, 'base64');
-        imageFile = new File([imageBuffer], "image.png", { type: "image/png" });
-      } else if (image.startsWith('http')) {
-        // Handle image URL
-        const response = await fetch(image);
-        const arrayBuffer = await response.arrayBuffer();
-        const buffer = Buffer.from(arrayBuffer);
-        imageFile = new File([buffer], "image.png", { type: response.headers.get("content-type") || "image/png" });
-      }
+      imageFile = await getImage(image);
     } else {
-      // Use default image if no image is provided
-      const imagePath = "images/COLLECTOR-quest-intro-1024.png";
-      const imageBuffer = fs.readFileSync(imagePath);
-      imageFile = new File([imageBuffer], "image.png", { type: "image/png" });
+      imageFile = await getImage("https://collectorquest.ai/images/COLLECTOR-quest-intro-1024.png");
     }
 
     // Generate the image
     const imageResponse = await openai.images.edit({
       model: "gpt-image-1",
       image: imageFile,
-      mask: mask,
+      mask: maskFile,
       prompt: `replace the quest master, keeping the style of the image, with a ${prompt}`,
       n: 1,
       size: "1024x1024",
