9ce8232fef5b05e341cb3e393630e7f48a34a3a3
John Gulbronson <john@benzinga.com>
2025-05-23 00:44:10 -0400

    update

---
+++ src/types/mysten.d.ts
@@ -0,0 +1,6 @@
+declare module '@mysten/wallet-adapter-react' {
+  export * from '@mysten/wallet-adapter-react/dist/index.d.ts';
+  export const WalletProvider: any;
+  export const createWalletKit: any;
+  export const getWallets: any;
+} 
