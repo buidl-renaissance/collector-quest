0522664d7bafb5b7626d31ed47d1139097cb8ddd
John Gulbronson <john@benzinga.com>
2025-06-05 19:39:14 -0400

    items

---
+++ migrations/20250605081844_items.js
@@ -0,0 +1,28 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  return knex.schema.createTable('items', table => {
+    table.string('id').primary();
+    table.string('name').notNullable();
+    table.string('objectId');
+    table.string('owner');
+    table.text('description');
+    table.string('imageUrl');
+    table.boolean('isActive').defaultTo(true);
+    table.jsonb('attributes');
+    table.jsonb('properties');
+    table.text('lore');
+    table.string('type');
+    table.timestamps(true, true);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('items');
+};
